fd1f31082497e3149afb86d518c62cae1187ce7f
vargardaikelys <vargasdaikelys@gmail.com>
2026-04-23 21:11:34 -0400

    creando proyecto

---
+++ src/Config/database.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Config;
+use PDO;
+use PDOException;
+class Database {
+private $host = "localhost";
+private $dbname = "uptaeb_proyectos";
+private $user = "root";
+private $pass = "";
+public $conn;
+public function getConnection() {
+$this->conn = null;
+try {
+$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . 
+$this->dbname, $this->user, $this->pass);
+$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+echo "Error de conexión: " . $e->getMessage();
+}
+return $this->conn;
+}
+}
